74e2e8ccc64154f9124c23c467c8d5dc3643fd10
Peter Nzingu <peterkyalo69@gmail.com>
2025-08-03 22:03:13 +0300

    Created a dynamic admin dashboard with register, login and logout functionality

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    // Admin Logout Method
+public function AdminLogout(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+    // End Admin Logout Method
+
+    
+}
